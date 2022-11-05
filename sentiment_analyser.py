e58e9769cc8b6466e32c184ab0b655c43ef57047
Sanath <62429581+SanathSame@users.noreply.github.com>
2022-11-06 02:22:14 +0800

    Update sentiment_analyser.py

--- sentiment_analyser.py
+++ sentiment_analyser.py
@@ -14,7 +14,7 @@ model = TFAutoModelForSequenceClassification.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
-if submit:
+if submit and user_input!=null:
     result = classifier(user_input)[0]
     label = result['label']
     score = result['score']
